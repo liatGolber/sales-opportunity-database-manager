fb3811a97a93012ea68448e2a0da66590ac9a043
Omer Avisror <omerav1@ac.sce.ac.il>
2018-11-26 01:01:53 +0200

    Add files via upload
    added databases as well as excel ref in code.

--- project 8/Program.cs
+++ project 8/Program.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace project_8
 {
+    
     static class Program
     {
+        public struct User
+        {
+            string name;
+            string ID;
+        }
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
